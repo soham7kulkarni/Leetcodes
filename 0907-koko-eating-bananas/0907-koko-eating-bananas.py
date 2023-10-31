1a77c392b92f5a27d6f04f6433cefa80ae059b65
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2023-10-31 15:41:12 -0700

    Time: 367 ms (46.96%), Space: 18 MB (20.24%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,7 +1,11 @@
+# max value of k will be max(piles). We need to traverse from 1 to max(piles) to check if the given value matches h. We will travers binarily. 
+# TC - O(log(max(p))*len(piles))
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = max(piles)
         low = 1
-        high = max(piles)
+        high = n
         res = high
         while(low <= high):
             mid = low + (high-low)//2
