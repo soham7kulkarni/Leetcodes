abde4d5ceb058a923d41413563ff32797b90fc87
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-02 08:29:50 -0800

    Time: 27 ms (97.81%) | Memory: 16.5 MB (76.74%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -16,38 +16,40 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-#         result = []
-#         self.dfs(root, 0, result)
-#         return result
-#     def dfs(self, root, depth, result):
-#         # base
-#         if not root: return
-#         # action
-#         if len(result) == depth:
-#             result.append(root.val)
-#         else:
-#             result[depth] = root.val
-#         # treverse left first then right
-#         self.dfs(root.left, depth+1, result)
-#         self.dfs(root.right, depth+1, result)
-from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        if not root: return result 
-        q = deque()    #Initiating deque/Linked List
-        q.append(root) #Adding root (depth = 0)
-        while q:
-            size = len(q)  #With popleft, length of deque decreses by 1 hence storing size before any modification
-            for i in range(0, size):
-                curr = q.popleft() #Take 1 node from deque
-                if i == size - 1: #If node is last then append it to the result since it is the rightmost element
-                    result.append(curr.val)
-                if curr.left: q.append(curr.left) #As it is not the rightmost go to his left and add left childrens
-                if curr.right: q.append(curr.right)  #As we are done with left, go to his right and add right childrens
+        self.dfs(root, 0, result)
         return result
+    def dfs(self, root, depth, result):
+        # base
+        if not root: return
+        # action
+        if len(result) == depth:
+            result.append(root.val)
+        else:
+            result[depth] = root.val
+        # treverse left first then right
+        self.dfs(root.left, depth+1, result)
+        self.dfs(root.right, depth+1, result)
+
+
+# from collections import deque
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         result = []
+#         if not root: return result 
+#         q = deque()    #Initiating deque/Linked List
+#         q.append(root) #Adding root (depth = 0)
+#         while q:
+#             size = len(q)  #With popleft, length of deque decreses by 1 hence storing size before any modification
+#             for i in range(0, size):
+#                 curr = q.popleft() #Take 1 node from deque
+#                 if i == size - 1: #If node is last then append it to the result since it is the rightmost element
+#                     result.append(curr.val)
+#                 if curr.left: q.append(curr.left) #As it is not the rightmost go to his left and add left childrens
+#                 if curr.right: q.append(curr.right)  #As we are done with left, go to his right and add right childrens
+#         return result
 
 
         
