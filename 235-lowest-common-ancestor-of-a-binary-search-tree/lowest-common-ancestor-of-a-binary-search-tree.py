073ceab80c6998fb324e319c2ca257d869cde954
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-04 19:38:46 -0800

    Time: 50 ms (90.23%) | Memory: 20.5 MB (76.01%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,8 +1,3 @@
-# Approach - Both P and Q less, go to left. Both are great, go to right. If found root, return root. 
-# If we found P and Q on LHS and RHS o root, return root.
-# TC - O(H), SC - O(H)
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -12,7 +7,7 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root.val > p.val and root.val > q.val: return self.lowestCommonAncestor(root.left, p , q)
-        elif root.val < p.val and root.val < q.val: return self.lowestCommonAncestor(root.right, p, q)
+        if p.val < root.val and q.val < root.val: return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val: return self.lowestCommonAncestor(root.right, p, q)
         else: return root
         
