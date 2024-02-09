38f55e39af5e492f41e8987c50a89cd27028c9f0
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-09 10:14:51 -0800

    Time: 46 ms (81.20%) | Memory: 18 MB (90.38%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,7 +1,7 @@
+# Approach - DFS. Connecting leaves with each other and if next node is present in same level then connecting sub trees
+# Preorder traversal
 # TC - O(N)
 # SC - O(1)
-# Approach - Two pointers. Level pointer for level traversal and curr pointer. 
-# Utilizing next pointer of the nodes
 """
 # Definition for a Node.
 class Node:
@@ -15,17 +15,13 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root: return root
-        level = root #Assigning root to level pointer
-        while level.left: #We dont want to traverse leaf node only parent nodes
-            curr = level #Assigning start of every level to curr pointer
-            while curr:
-                curr.left.next = curr.right #if curr exist, both leaves exist, linking them
-                if curr.next: #if 2 and 3 both exist, then linking between two subtrees
-                    curr.right.next = curr.next.left
-                curr = curr.next #traversing to next curr from 2 to 3
-            level = level.left #After 3, level completed hence moving to next level i.e left of level
+        self.preorder(root)
         return root
-            
-                
-
+    def preorder(self, root):
+        if not root.left: return
+        root.left.next = root.right
+        if root.next:
+            root.right.next = root.next.left
+        self.preorder(root.left)
+        self.preorder(root.right)
         
