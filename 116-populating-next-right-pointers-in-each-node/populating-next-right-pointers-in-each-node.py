2e6497ea22a8bd0320c148a9b9840ee5b07e4d2e
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-09 00:20:44 -0800

    Time: 51 ms (57.26%) | Memory: 18 MB (71.74%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,3 +1,5 @@
+# TC - O(N)
+# SC - O(N)
 """
 # Definition for a Node.
 class Node:
@@ -10,24 +12,23 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root: return root
-        q = deque([root])
+        if not root: return root  #for null case, we will return empty array
+        q = deque([root])   #add root to the linked list
         
-        while q:
-            size = len(q)
-            prev = None
-            for _ in range(size):
-                curr = q.popleft()
+        while q:    #We will traverse all the nodes from the current level
+            size = len(q)  #For level 0 - size:1, level 1 -  size:2, level 2 - size:4
+            prev = None  #Cant link root as successor of any node
+            for _ in range(size):  #traversing through all nodes
+                curr = q.popleft() #Taking 1 at a time
                 
-                if prev:
-                    prev.next = curr
+                if prev:   #If we have prev then only we will link our current as next pointer of prev
+                    prev.next = curr  #Linking 
                 
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
+                if curr.left:  #If we have left node then right exist for sure since its perfect binary tree
+                    q.append(curr.left)  #Add left child to the queue
+                    q.append(curr.right) #Add right child to the queue
                 
-                prev = curr
+                prev = curr  #We auto increment and consider next node in queue hence updating prev to root is imp
                 
         return root
 
