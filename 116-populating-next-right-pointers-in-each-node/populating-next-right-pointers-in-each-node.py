5dc41ab2017cb728028f69d66cb3f077f76fdba4
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-09 10:02:10 -0800

    Time: 40 ms (95.36%) | Memory: 17.9 MB (90.38%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,5 +1,7 @@
 # TC - O(N)
-# SC - O(N)
+# SC - O(1)
+# Approach - Two pointers. Level pointer for level traversal and curr pointer. 
+# Utilizing next pointer of the nodes
 """
 # Definition for a Node.
 class Node:
@@ -12,24 +14,18 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root: return root  #for null case, we will return empty array
-        q = deque([root])   #add root to the linked list
-        
-        while q:    #We will traverse all the nodes from the current level
-            size = len(q)  #For level 0 - size:1, level 1 -  size:2, level 2 - size:4
-            prev = None  #Cant link root as successor of any node
-            for _ in range(size):  #traversing through all nodes
-                curr = q.popleft() #Taking 1 at a time
-                
-                if prev:   #If we have prev then only we will link our current as next pointer of prev
-                    prev.next = curr  #Linking 
-                
-                if curr.left:  #If we have left node then right exist for sure since its perfect binary tree
-                    q.append(curr.left)  #Add left child to the queue
-                    q.append(curr.right) #Add right child to the queue
-                
-                prev = curr  #We auto increment and consider next node in queue hence updating prev to root is imp
-                
+        if not root: return root
+        level = root #Assigning root to level pointer
+        while level.left: #We dont want to traverse leaf node only parent nodes
+            curr = level #Assigning start of every level to curr pointer
+            while curr:
+                curr.left.next = curr.right #if curr exist, both leaves exist, linking them
+                if curr.next: #if 2 and 3 both exist, then linking between two subtrees
+                    curr.right.next = curr.next.left
+                curr = curr.next #traversing to next curr from 2 to 3
+            level = level.left #After 3, level completed hence moving to next level i.e left of level
         return root
+            
+                
 
         
