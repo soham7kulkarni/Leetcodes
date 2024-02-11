96f23c43f3beab0bb05d37e7653b7d9d472b037a
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-10 16:48:06 -0800

    Time: 124 ms (51.32%) | Memory: 23.5 MB (95.95%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -1,7 +1,3 @@
-# Approach - DFS. Move to left if the root value is greater than low
-# Move to right if root value is less than high
-# TC - O(n), SC - O(1)
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,22 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.result = 0
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if not root: return 0
-        self.dfs(root, low, high)
-        return self.result
+        return self.dfs(root, low ,high)
 
-    def dfs(self, root, low, high):
-        if not root: return
+    def dfs(self, root, low, high) -> int:
+        if not root: return 0
+        current = 0
         if root.val >= low and root.val <= high:
-            self.result += root.val
+           current = root.val
+        
+        left = self.dfs(root.left, low, high)
+        right = self.dfs(root.right, low, high)
 
-        if root.val > low:  self.dfs(root.left, low, high)
-       
-        if root.val < high: self.dfs(root.right, low ,high)
+        return current+left+right
 
-        return self.result
-    
         
