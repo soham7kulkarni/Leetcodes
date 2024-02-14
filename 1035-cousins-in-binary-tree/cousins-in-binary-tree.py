d8d29b6885f4e280c6017f654de4468b0498e11f
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-14 10:26:53 -0800

    Time: 42 ms (28.55%) | Memory: 16.6 MB (61.88%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -5,34 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    depthX = 0
-    depthY = 0
-    parentX = None
-    parentY = None
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        self.dfs(root, x, y, 0, None)
-        if self.parentX != self.parentY and self.depthX == self.depthY: return True
+        q = deque()
+        pq = deque()
+        px, py = None, None
+        x_found, y_found = False, False
+        q.append(root)
+        pq.append(None)
+        while q:
+
+            n = len(q)
+            for i in range(n):
+                curr = q.popleft()
+                parent = pq.popleft()
+                if curr.val == x:
+                    x_found = True
+                    px = parent
+                if curr.val == y:
+                    y_found = True
+                    py = parent
+                if curr.left:
+                    q.append(curr.left)
+                    pq.append(curr)
+                if curr.right:
+                    q.append(curr.right)
+                    pq.append(curr)
+
+            if x_found and y_found: return px!= py
+            if x_found or y_found: return False
+            
         return False
-    
-    def dfs(self, root, x, y, depth, parent):
-        # base
-        if not root: return
-
-        # logic
-
-        # x
-        if root.val == x:
-            self.parentX = parent
-            self.depthX = depth
-        # y
-        if root.val == y:
-            self.parentY = parent
-            self.depthY = depth
-
-        # optimize
-        if self.parentX is None or self.parentY is None:
-            self.dfs(root.left, x, y, depth+1, root)
-        if self.parentX is None or self.parentY is None:
-            self.dfs(root.right, x, y, depth+1, root)
-        
+                
         
