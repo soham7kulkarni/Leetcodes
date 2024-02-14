696b9d0746cdae80cffaf9d451aca2a7e9884357
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-14 10:00:08 -0800

    Time: 39 ms (46.73%) | Memory: 16.6 MB (82.71%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -10,20 +10,29 @@ class Solution:
     parentX = None
     parentY = None
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        
-        self.dfs(root, 0, None, x, y)
-        if self.depthX == self.depthY and self.parentX != self.parentY: return True
+        self.dfs(root, x, y, 0, None)
+        if self.parentX != self.parentY and self.depthX == self.depthY: return True
         return False
-        
-    def dfs(self, root, depth, parent, x, y):
+    
+    def dfs(self, root, x, y, depth, parent):
+        # base
         if not root: return
+
+        # logic
+
+        # x
         if root.val == x:
             self.parentX = parent
             self.depthX = depth
+        # y
         if root.val == y:
             self.parentY = parent
             self.depthY = depth
+
+        # optimize
         if self.parentX is None or self.parentY is None:
-            self.dfs(root.left, depth+1, root, x, y)
+            self.dfs(root.left, x, y, depth+1, root)
         if self.parentX is None or self.parentY is None:
-            self.dfs(root.right, depth+1, root, x, y)
+            self.dfs(root.right, x, y, depth+1, root)
+        
+        
