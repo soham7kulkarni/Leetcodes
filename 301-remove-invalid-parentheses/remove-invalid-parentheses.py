a5f5309513e63231dc69d89e87d7165c9f22dddc
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-15 10:24:53 -0800

    Time: 225 ms (57.80%) | Memory: 18.5 MB (15.76%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.py
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.py
@@ -1,40 +1,34 @@
-# Approach BFS - Remove 1 parenthesis at a time and 
-# abondon the children (posiible combination without it) inside queue.
-# Check if we get possible output and 
-# keep on removing 1 parenthesis till we get possible answers
-
-# Approahc DFS - Remove 1 parenthesis at a time and maintain possible outcomes and 
-# max length. If we get bigger outcome we replace or keep it alongside the other outcomes.
-# We maintain hashsets to avoid processing the same sequence.
-
-# TC - O(N^N) SC - O(N^N)
 class Solution:
-    def __init__(self):
-        self.max = 0 # max length of the valid substring
-        self.result = [] #to hold the output
-        self.set = set() # for not repating the already processed subsequence
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        
-        self.dfs(s)
-        return self.result
-
-    def dfs(self, s):
-        if len(s) < self.max: return #If we have output len = 5 why check substring of 4 characters
-        if self.isValid(s):
-            if len(s) > self.max:
-                self.result = [] #Discarding previous short subsequence
-                self.max = len(s)
-            self.result.append(s) #Adding it to output
-
-        # logic
-        for i in range(len(s)):
-            c = s[i]
-            if c != '(' and c != ')': continue #if its character continue
-            child = s[:i] + s[i+1:] #subsequence without that particular char
-            if child not in self.set: #If child not present in set then only add
-            # and process or else just skip that child
-                self.set.add(child)
-                self.dfs(child) 
+        result = []
+        if not s:  # Check if the input string is empty
+            return result
+        if self.isValid(s): # If the give input is itself valid
+            return [s]
+        sett = set()
+        q = deque()
+        q.append(s)
+        sett.add(s)
+        found = False # Flag to check if we have already found the 
+        # bigger valid string
+        while q and not found:
+            size = len(q)
+            for k in range(size): # To form the child of 
+            # every string in the queue
+                curr = q.popleft()
+                if self.isValid(curr):
+                    # Input is 5 char and we formed 4 char child
+                    # If we found one of them is valid we will not produce
+                    # Child of other strings. Just check if to append or not
+                    result.append(curr)
+                    found = True
+                if not found:
+                    for i in range(len(curr)):
+                        child = curr[:i] + curr[i+1:]
+                        if child not in sett:
+                            sett.add(child)
+                            q.append(child)
+        return result
 
     def isValid(self, s) -> bool:
         count = 0
@@ -46,8 +40,3 @@ class Solution:
                 if count == 0: return False
                 count-=1
         return count == 0
-
-
-
-
-        
