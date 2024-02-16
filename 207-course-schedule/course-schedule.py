a6d35007986e38a9d4ec3c18766adf5e7de8f075
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-16 11:52:34 -0800

    Time: 92 ms (55.84%) | Memory: 18.4 MB (67.84%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,39 @@
+# Approach: BFS with Indegree to maintain no. of prereqs and hashmap for list of dependents
+# We don't  maintain size because we are focusing on completion of courses and not on no. of semesters
+# TC - O(V+E)
+# SC - O(V+E)
+
 from collections import defaultdict, deque
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         mapp = defaultdict(list)
-        indegree = [0] * numCourses
+        indegree = [0] * numCourses # Indegree to maintain no. of prereqs
+
 
         for edge in prerequisites:
             dependent, indepe = edge
-            indegree[dependent] += 1
-            mapp[indepe].append(dependent)
+            indegree[dependent] += 1 #Update indegree array
+            mapp[indepe].append(dependent) #Append dependent to the list
 
         q = deque()
-        count = 0
+        count = 0 # To check if we have processed all courses or not
 
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if indegree[i] == 0: # start with independent node
                 q.append(i)
                 count += 1
 
         if not q:
-            return False
+            return False #it means cycle is present, return false
 
         while q:
             course = q.popleft()
-            children = mapp[course]
+            children = mapp[course] #Go through the children of this node (BFS)
             for child in children:
-                indegree[child] -= 1
+                indegree[child] -= 1 # Reduce dependency count by 1
                 if indegree[child] == 0:
-                    q.append(child)
+                    q.append(child) # Append any independent node to queue
                     count += 1
 
         return count == numCourses
