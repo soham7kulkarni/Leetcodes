82d9b67ac522a332da594213edf7154990af333f
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-20 09:06:18 -0800

    Time: 63 ms (82.17%) | Memory: 16.6 MB (88.17%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,4 +1,4 @@
-# Approach - BFS. Appending seperate row and column value to q
+# Approach - DFS
 # TC - O(M*N)
 # SC - O(M*N)
 class Solution:
@@ -6,23 +6,20 @@ class Solution:
         m = len(image)
         n = len(image[0])
         dirs = [[0,-1],[-1,0],[0,1],[1,0]]
-        q = deque()
         orig = image[sr][sc]
-        image[sr][sc] = color
-        q.append(sr)
-        q.append(sc)
-        while q:
-            cr = q.popleft()
-            cc = q.popleft()
-            if orig == color: return image
-            for d in dirs:
-                nr = cr + d[0]
-                nc = cc + d[1]
-                if 0 <= nr < m and 0 <= nc < n:
-                    if image[nr][nc] == orig:
-                        image[nr][nc] = color
-                        q.append(nr)
-                        q.append(nc)
+        if orig == color: return image
+        self.dfs(image, sr, sc, orig, color, dirs)
         return image
 
-        
+    def dfs(self, image, sr, sc, orig, newColor, dirs):
+        # base
+        if sr < 0 or sr == len(image) or sc < 0 or sc == len(image[0]) or image[sr][sc] != orig: return  # We check if the node is already visited by
+        # comapring its original value with current value. If its visited then
+        # value will be 2 and not 1
+
+        # logic
+        image[sr][sc] = newColor
+        for d in dirs:
+            nr = sr + d[0]
+            nc = sc  + d[1]
+            self.dfs(image, nr, nc, orig, newColor, dirs)
