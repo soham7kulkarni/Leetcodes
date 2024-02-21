7890293726236c9074e25f574e62cf155a0ec89b
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-21 14:51:32 -0800

    Time: 240 ms (68.97%) | Memory: 18.9 MB (77.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,6 +1,9 @@
 # Approach - BFS
 # TC - O(M*N)
+# EXACT - 2(MN) In worst case (all 1s), We convert every 1 to 0 in first element 
+# for loop. But still we check all 0s to complete the for loop hence we touch every element at least twice.
 # SC - O(M*N)
+# EXACT - min(m,n) Consider skew matrix and then check the length of the longest diagonal
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
