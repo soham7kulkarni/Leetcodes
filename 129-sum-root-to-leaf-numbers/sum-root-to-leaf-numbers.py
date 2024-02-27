68df0a01ace372271fd6e856a154ef774c93ec56
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-27 07:38:12 -0800

    Time: 32 ms (79.71%) | Memory: 16.6 MB (51.22%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,10 +1,3 @@
-# Int based recursion
-# TC - O(n)
-# SC - O(h) - O(N) when skewed
-# Approach - For every root(node), calculate LHS and RHS sum and then add it. When we hit the leaf, stop the number formation and just return the formed number.
-
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,24 +6,22 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.currSum = 0
+        self.result = 0
 
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        return self.helper(root, 0)
+        if not root: return self.result
+        self.dfs(root, 0)
+        return self.result
 
-    def helper(self, root: Optional[TreeNode], currNum: int) -> int:
-
+    def dfs(self, root, num):
         # base
-        if root is None: return 0
-
+        if not root: return
         # logic
-        currNum = currNum*10 + root.val
-        
-        left = self.helper(root.left, currNum)
-        # st.pop()
-        if root.left is None and root.right is None: return currNum
-        right = self.helper(root.right, currNum)
-        # st.pop()
-        return left + right
-        
+        num = num*10 + root.val
+        if root.left is None and root.right is None: self.result += num
+        self.dfs(root.left, num)
+        self.dfs(root.right, num)
+
+
+
         
