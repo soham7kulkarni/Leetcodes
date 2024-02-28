06b9ac10c429e32585457718419e0a8385fc93e3
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-28 10:33:56 -0800

    Time: 906 ms (5.00%) | Memory: 445.2 MB (5.03%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,11 +1,3 @@
-# Approach (DFS) - Record paths for p and q from root. Backtrack and pop all the leaf nodes from the path.
-# As we have pathP = [3,5] pathQ = [3,1]. We will check two arrays and if at 'i' the elements are different,
-# We return i-1 element as it will be LCA.
-# Since two paths can be of different length, we add root twice so that we dont run out of array.
-# pathP = [3,5,5], pathQ = [3,1,1], we return 3
-
-# TC - O(N), SC - O(H)
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -18,20 +10,15 @@ class Solution:
         self.pathP = []
         self.pathQ = []
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-
-
         self.backtrack(root, p, q, path)
-        # I have the paths
         for i in range(len(self.pathP)):
             if self.pathP[i] != self.pathQ[i]:
                 return self.pathP[i-1]
 
         return None
-    
+
     def backtrack(self, root, p, q, path):
-        # base
-        if not root: return 
-        # action
+        if not root: return
         path.append(root)
         if root == p:
             self.pathP = path[:]
@@ -39,13 +26,9 @@ class Solution:
         if root == q:
             self.pathQ = path[:]
             self.pathQ.append(root)
-        # traverse
-        self.backtrack (root.left, p ,q, path.copy())
-        self.backtrack (root.right, p ,q, path.copy())
-        # backtrack
+
+        self.backtrack(root.left, p, q, path.copy())
+        self.backtrack(root.right, p, q, path.copy())
+
         path.pop()
-
-        
-
-
         
