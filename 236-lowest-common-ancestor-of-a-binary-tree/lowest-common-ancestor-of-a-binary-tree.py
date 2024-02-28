b5581169f74659f32621c46df9af5cbe9664124b
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-02-28 11:10:25 -0800

    Time: 58 ms (24.86%) | Memory: 21 MB (83.72%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -6,29 +6,22 @@
 #         self.right = None
 
 class Solution:
-    def __init__(self):
-        self.pathP = []
-        self.pathQ = []
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.backtrack(root, p, q, path)
-        for i in range(len(self.pathP)):
-            if self.pathP[i] != self.pathQ[i]:
-                return self.pathP[i-1]
+        # base
+        if root is None or p is None or q is None: return root
+        # Recursive logic
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if root == p or root == q:
+            return root
+        if left is None and right is None:
+            return None
+        elif left is not None and right is None:
+            return left
+        elif left is None and right is not None:
+            return right
+        else:
+            return root
 
-        return None
-
-    def backtrack(self, root, p, q, path):
-        if not root: return
-        path.append(root)
-        if root == p:
-            self.pathP = path[:]
-            self.pathP.append(root)
-        if root == q:
-            self.pathQ = path[:]
-            self.pathQ.append(root)
-
-        self.backtrack(root.left, p, q, path.copy())
-        self.backtrack(root.right, p, q, path.copy())
-
-        path.pop()
         
