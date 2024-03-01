816f01fb1cc50c1666bce549d752a3595605e5b6
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-03-01 15:53:53 -0800

    Time: 45 ms (96.93%) | Memory: 17.9 MB (97.47%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,23 +6,17 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        self.map = {val: idx for idx, val in enumerate(inorder)}
-        self.idx = 0
+        self.mapp = {val: idx for idx, val in enumerate(inorder)}
+        self.index = 0
         return self.helper(preorder, 0, len(preorder)-1)
-    
+
     def helper(self, preorder, start, end):
-        # base
         if start > end:
             return None
-
-        root_val = preorder[self.idx] 
+        root_val = preorder[self.index]
         root = TreeNode(root_val)
-        root_idx = self.map[root_val]
-        self.idx += 1
-        root.left = self.helper(preorder, start, root_idx - 1)
-        root.right = self.helper(preorder, root_idx + 1, end)
+        root_idx = self.mapp[root_val]
+        self.index += 1
+        root.left = self.helper(preorder, start, root_idx-1)
+        root.right = self.helper(preorder, root_idx+1, end)
         return root
-
-
-
-        
