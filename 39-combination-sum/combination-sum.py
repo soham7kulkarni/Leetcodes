c6763b4f2d9f635d724bde56970370be9c561175
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-03-02 15:03:04 -0800

    Time: 54 ms (53.72%) | Memory: 16.5 MB (89.78%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,22 @@
 class Solution:
     def __init__(self):
         self.result = []
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         if not candidates: return self.result
         self.helper(candidates, target, 0, [])
         return self.result
 
     def helper(self, candidates, target, pivot, path):
-        if target < 0 or pivot == len(candidates): return 
+        #    base
         if target == 0:
-            self.result.append(path)
+            self.result.append(path[:])
+            return
+        if target < 0 or pivot == len(candidates):
             return
 
         for i in range(pivot, len(candidates)):
-            li = path[:]
-            li.append(candidates[i])
-            self.helper(candidates, target-candidates[i], i, li)
-
+            path.append(candidates[i])
+            self.helper(candidates, target - candidates[i], i, path)
+            path.pop()
+        
