5e579bcca089655aa8b1b3e884908a7e3ccb3b5b
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-03-02 14:35:37 -0800

    Time: 55 ms (49.42%) | Memory: 16.5 MB (89.78%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,28 +1,19 @@
-# Approach - for loop backtrack but creating new list at every node
-
 class Solution:
     def __init__(self):
         self.result = []
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         if not candidates: return self.result
-        self.helper(candidates, 0, target, [])
+        self.helper(candidates, target, 0, [])
         return self.result
 
-    def helper(self, candidates, pivot, target, path):
-        # base
+    def helper(self, candidates, target, pivot, path):
+        if target < 0 or pivot == len(candidates): return 
         if target == 0:
-            self.result.append(path) # If we return path then it will have all the elements
-            # from the path we traversed.
-            # Hence creating deep copy of the path and return is necessary 
+            self.result.append(path)
             return
-        if pivot == len(candidates) or target < 0: return
-
-        # logic
 
         for i in range(pivot, len(candidates)):
             li = path[:]
             li.append(candidates[i])
-            self.helper(candidates, i ,target - candidates[i],li)
-           #                      pivot moves from i onward
+            self.helper(candidates, target-candidates[i], i, li)
 
-        
