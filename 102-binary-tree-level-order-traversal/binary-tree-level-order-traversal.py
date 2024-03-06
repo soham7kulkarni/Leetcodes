6a66b809edd31761ddc5e0d41b6ff8fe328038d1
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-03-05 16:01:48 -0800

    Time: 42 ms (46.35%) | Memory: 17.3 MB (64.58%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -1,10 +1,3 @@
-# Approach - DFS
-# depth == size, True: there no exist any node for particular depth. 
-# Create one list and append
-# False: Already nodes are present at that depth level(processed). Just append
-# TC - O(N)
-# SC - O(H) Stack of recursive calls
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -12,19 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.result = []
-
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return self.result
-        self.dfs(root, 0)
-        return self.result
-
-    def dfs(self, root, depth):
-        if not root: return
-        if depth == len(self.result):
-            self.result.append([])
-        self.result[depth].append(root.val)
-        self.dfs(root.left, depth + 1)
-        self.dfs(root.right, depth + 1)
-        
+        q = deque()
+        result = []
+        if not root: return result
+        q.append(root)
+        while q:
+            li = []
+            size = len(q)
+            for i in range(size):
+                curr = q.popleft()
+                li.append(curr.val)
+                if curr.left: q.append(curr.left)
+                if curr.right: q.append(curr.right)  
+            result.append(li)
+        return result      
