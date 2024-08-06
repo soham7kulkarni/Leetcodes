db888abf3512858b5f02d4e8130169b873b0fbba
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-08-06 09:03:51 -0700

    Time: 90 ms (18.35%) | Memory: 17.9 MB (9.89%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,3 +1,15 @@
+# Its important to consider the exactly all cases and determine when collision will occur and it wont
+# In this case, if positive asteroid is the next one, there will never be a collision
+# Also, stack can have mix of positive and negative asteroids. 
+# Assumption: stack will have only positives or negatives is wrong
+
+# TC - O(2N) = O(N)
+# Although we have nested while inside for loop, we push or pop each asteroid max once. 
+# So in case [5,10,-15,-20], when -20 is anticipated, only -15 is present in stack
+# Hence assuming traversing over all elements of stack will result in O(N) is not necessarily true 
+# And stack operations which dominate the while loop are of linear complexity 
+
+
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         st = []
