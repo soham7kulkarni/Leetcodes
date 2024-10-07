549a99e7c4e750614f34a7f8e98e3375670d82c8
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-07 10:40:07 -0700

    Time: 39 ms (48.64%) | Memory: 17.4 MB (82.83%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -6,29 +6,45 @@ class Node:
         self.next = next
         self.random = random
 """
+# TC - O(2N)
+# SC - O(N)
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # Base case
         if not head:
             return None
-        node_map = {}
+        # Creating deep copy of head
         deepCopyHead = Node(head.val)
-        node_map[head] = deepCopyHead
-        curr = head.next
+        # creating dictionary. It will store original nodes along
+        # with deep copies of original nodes
+        node_map = {}
+        # Using curr to traverse thru original LL
+        curr = head
+        # Putting first node i.e head inside map
+        node_map[curr] = deepCopyHead
+        # Going to next 
+        curr = curr.next
+        # Preparing copyCurr for traversal thru deep copies
         copyCurr = deepCopyHead
         while curr:
+            # creating deep copy of curr to put in dictionary
+            # then linking it as a next node of copyCurr
             newNode = Node(curr.val)
             node_map[curr] = newNode
-            curr = curr.next
             copyCurr.next = newNode
+            # Traversing to next node
+            curr = curr.next
             copyCurr = copyCurr.next
+        # We need to link all random pointers of all deep copies
         curr = head
         copyCurr = deepCopyHead
         while curr:
+            # if curr has random
             if curr.random:
+                # linking random of copyCurr 
+                # with deep copy of node pointed by random of curr
                 copyCurr.random = node_map[curr.random]
             curr = curr.next
             copyCurr = copyCurr.next
         return deepCopyHead
-        
-        
