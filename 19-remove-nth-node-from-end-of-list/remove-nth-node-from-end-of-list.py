706c4080dd7ad9b19dcb1717787b3c7715ea0015
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-07 07:57:15 -0700

    Time: 35 ms (68.88%) | Memory: 16.5 MB (28.36%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,3 +1,12 @@
+# Take two pointers fast and slow. 
+# Fast pointer will traverse n times in first for loop. 
+# In the second for loop, bith fast and slow pointer will increment by 1.  
+# At this time, there is gap of n nodes in both pointers. When fast pointer approaches the end, slow pointer is on nth node. 
+# Delete it by linking node.next = node.next.next. Point dummy at head. Return head
+# Edge case - where you want to remove first node. Suppose list of 2 nodes and remove 2nd node from last i.e. first node. 
+# In this case, after first for loop, fast is null. Hence we jsut return 2nd node as head by omitting 1st node instead of deleting.
+# TC - O(N), SC - O(1)
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,10 +16,12 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         if not head:
             return None
-
-        slow = fast = head
-        for i in range(0,n):
+        slow = head
+        fast = head
+        count = 0
+        while count < n:
             fast = fast.next
+            count += 1
         if not fast:
             return head.next
         while fast.next:
