0d8704fdbff932783135bbdab35b1f2304e8d901
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-10 14:14:16 -0700

    Time: 229 ms (84.11%) | Memory: 18.8 MB (85.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,33 @@
-# Approach - DFS
+# Approach - BFS
 # TC - O(M*N)
+# EXACT - 2(MN) In worst case (all 1s), We convert every 1 to 0 in first pass 
+# for loop. But still we check all 0s to complete the for loop 
+# hence we touch every element at least twice.
 # SC - O(M*N)
-
+# EXACT - min(m,n) Consider skew matrix and then check the length of the longest diagonal
+# Not necessarily length of diagonal but the number of elements
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        dirs = [[0,-1], [0,1], [-1,0], [1,0]]
         count = 0
+        queue = deque()
+        dirs = [[0,1], [0,-1], [1, 0], [-1, 0]]
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
+                if grid[i][j] == '1':
                     count += 1
-                    self.dfs(grid, i, j, m, n, dirs)
+                    queue.append((i, j))
+                    while queue:
+                        curr = queue.popleft()
+                        for direction in dirs:
+                            nr = curr[0] + direction[0]
+                            nc = curr[1] + direction[1]
+                            if nr >= 0 and nr < m and nc >= 0 and nc < n and grid[nr][nc] == '1':
+                                grid[nr][nc] = '0'
+                                queue.append((nr, nc))
         return count
 
-    def dfs(self, grid, i, j , m, n, dirs):
-        # base
-        if i < 0 or j < 0 or i == m or j == n or grid[i][j] == "0": return
-        # logic
-        grid[i][j] = "0"
-        for d in dirs:
-            r = i + d[0]
-            c = j + d[1]
-            self.dfs(grid, r, c, m, n, dirs)
-       
-                    
 
         
