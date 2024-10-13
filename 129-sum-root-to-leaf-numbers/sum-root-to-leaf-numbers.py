77ae41e835e26ce593490a1ba3e616a693de082e
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-13 08:47:37 -0700

    Time: 39 ms (27.55%) | Memory: 16.5 MB (39.27%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,4 +1,7 @@
-# Approach - void based inorder
+# Approach - int based preorder
+# children passing values to root
+# root having left sum and right sum
+# root returning sum of both ends
 # TC - O(N)
 # SC - O(h)
 
@@ -9,20 +12,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self) -> None:
-        self.currSum = 0
+    
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
-        self.helper(root, 0)
-        return self.currSum
-    def helper(self, root, currNum) -> None:
+        return self.helper(root, 0)
+    def helper(self, root, currNum) -> int:
         # base
-        if not root: return
+        if not root: return 0
         # logic
         currNum = currNum*10 + root.val
-        self.helper(root.left, currNum)
         if not root.left and not root.right:
-            self.currSum += currNum
-            return
-        self.helper(root.right, currNum)
+            return currNum
+        left = self.helper(root.left, currNum)
+        
+        right = self.helper(root.right, currNum)
+
+        return left + right
         
