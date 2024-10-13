644b7bf41318d6b3adf9c163cf249447691d26e5
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-13 15:53:21 -0700

    Time: 586 ms (96.74%) | Memory: 21.6 MB (62.79%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,16 +1,17 @@
+# We maintain the indegrees for all nodes inside the array. 
+# If node1 trusts node2, indegree of node1 will decrease by 1 and node2 will increase by 1. 
+# This way, we solve both problems, if judge is trusting nobody then he should have indegree n-1.
+# Time complexity, O(V+E) since it might happen that v is greater than edges.
+# Space complexity O(V)
+
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        s = set()
-        mapp = {}
-        for k in range(1, n+1):
-            mapp[k] = 0
-        for i,j in trust:
-            s.add(i)
-            mapp[j]+=1
-        for l in mapp:
-            if mapp[l] == n-1 and l not in s:
-                return l
+        indegrees = [0]*(n+1)
+        for a,b in trust:
+            indegrees[a] -= 1 
+            indegrees[b] += 1
+        for i in range(1, n+1):
+            if indegrees[i] == n-1:
+                return i
         return -1
-
-
         
