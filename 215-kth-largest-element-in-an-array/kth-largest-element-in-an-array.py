39c58bcb1eaa7408e48385c2200a688789ba13c0
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-13 13:05:52 -0700

    Time: 500 ms (22.16%) | Memory: 29.8 MB (37.11%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -12,12 +12,22 @@
 # TC - O(nlogk)
 # SC - O(k)
 
+# Approach 3 - Using max heap. But now we maintain n-k elements
+# These are all small elements but with largest element at the top.
+# Whenever we exceed n-k size of heap, we pop that element (it will be largest)
+# Compare it with result ele which stores largets element
+# Since we maintain n-k, we always deal with kth largest element and not largest
+# TC - O(nlog(n-k))
+# SC - O(n-k)
+
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        result = []
+        n = len(nums)
+        max_heap = []
+        result = float('inf')
         for num in nums:
-            heapq.heappush(result, num)
-            if len(result) > k:
-                heapq.heappop(result)
-        return result[0]
+            heapq.heappush(max_heap, -num)
+            if len(max_heap) > n-k:
+               result = min(result, -heapq.heappop(max_heap)) 
+        return result
