c47dcd50f86493b023c11485448fdd8c9edabec5
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-13 08:34:03 -0700

    Time: 45 ms (7.34%) | Memory: 16.5 MB (80.44%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,3 +1,7 @@
+# Approach - void based inorder
+# TC - O(N)
+# SC - O(h)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,23 +9,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.result = 0
-
+    def __init__(self) -> None:
+        self.currSum = 0
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        if not root: return self.result
-        self.dfs(root, 0)
-        return self.result
-
-    def dfs(self, root, num):
+        if not root: return 0
+        self.helper(root, 0)
+        return self.currSum
+    def helper(self, root, currNum) -> None:
         # base
         if not root: return
         # logic
-        num = num*10 + root.val
-        if root.left is None and root.right is None: self.result += num
-        self.dfs(root.left, num)
-        self.dfs(root.right, num)
-
-
-
+        currNum = currNum*10 + root.val
+        self.helper(root.left, currNum)
+        if not root.left and not root.right:
+            self.currSum += currNum
+            return
+        self.helper(root.right, currNum)
         
