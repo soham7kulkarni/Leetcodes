dadd40c707a34076d68aec5dd07f0cacc65211f2
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-14 11:18:14 -0700

    Time: 245 ms (57.16%) | Memory: 18.9 MB (85.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,16 +1,9 @@
-# Approach - BFS
-# TC - O(M*N)
-# EXACT - 2(MN) In worst case (all 1s), We convert every 1 to 0 in first pass 
-# for loop. But still we check all 0s to complete the for loop 
-# hence we touch every element at least twice.
-# SC - O(M*N)
-# EXACT - min(m,n) Consider skew matrix and then check the length of the longest diagonal
-# Not necessarily length of diagonal but the number of elements
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
+        i = 0
+        j = 0
         count = 0
         queue = deque()
         dirs = [[0,1], [0,-1], [1, 0], [-1, 0]]
@@ -18,16 +11,14 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == '1':
                     count += 1
-                    queue.append((i, j))
-                    while queue:
-                        curr = queue.popleft()
-                        for direction in dirs:
-                            nr = curr[0] + direction[0]
-                            nc = curr[1] + direction[1]
-                            if nr >= 0 and nr < m and nc >= 0 and nc < n and grid[nr][nc] == '1':
-                                grid[nr][nc] = '0'
-                                queue.append((nr, nc))
+                    self.bfs(i, j, m, n, grid, dirs)
         return count
-
+    def bfs(self, i, j, m, n, grid, dirs) -> None:
+        if i < 0 or i ==m or j < 0 or j == n or grid[i][j] == '0': return
+        grid[i][j] = '0'
+        for direction in dirs:
+            nr = i + direction[0]
+            nc = j + direction[1]
+            self.bfs(nr, nc, m, n, grid, dirs)
 
         
