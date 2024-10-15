03cf11046065ea698f172cd04d650758bf1d8cbf
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-15 14:10:10 -0700

    Time: 46 ms (71.07%) | Memory: 17 MB (30.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,18 @@
+# TC - O(N)
+# SC - O(1)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t): return False
 
-        sCount = {}
-        tCount = {}
+        if len(s) != len(t):
+            return False
+
+        s_dict = {}
+        t_dict = {}
 
         for i in range(len(s)):
-            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
         for j in range(len(t)):
-            tCount[t[j]] = 1 + tCount.get(t[j], 0)
-
-        for c in sCount:
-            if sCount[c] != tCount.get(c, 0):
-                return False
-
-        return True 
-
+            t_dict[t[j]] = 1 + t_dict.get(t[j], 0)
+        
+        return s_dict == t_dict
         
