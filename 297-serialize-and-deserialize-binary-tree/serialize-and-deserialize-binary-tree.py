8f274e39571e7bbb11efa2817ee5a09f030ac2ba
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-14 21:21:08 -0700

    Time: 85 ms (48.27%) | Memory: 20.5 MB (75.21%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -1,3 +1,11 @@
+# Approach: BFS
+# We have to add null(#) in serialization. It helps in keeping track of null
+# We can not just go deserialize the string with level 0 = 1 node ,level 1 = 2 node, level 3 = 8 node.
+# Its because the tree is not necessarily perfect binary tree.
+# Also iterator i is used to assign empty nodes to respective parents. We just don't create empty node. We skip them by incrementing i by 1.
+# TC: O(N), SC:O(N)
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -6,31 +14,34 @@
 #         self.right = None
 
 class Codec:
-    def __init__(self):
-        self.sb = ""
-        global i
-        i = 0
+
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
+        # If we don't have tree to serialize then return empty string
         if not root: return ""
-        self.dfs(root)
-        print(str(self.sb))
-        return self.sb
+        # For BFS level order tarversal
+        q = deque()
+        # Serializing the tree and using # in place of empty nodes
+        sb = ""
+        q.append(root)
+        while q:
+            curr = q.popleft()
+            if not curr:
+                sb+= "#"
+            else:
+                sb += str(curr.val)
+                # Adding children of the node to the queue
+                q.append(curr.left)
+                q.append(curr.right)
+        # For debugging: adding , and printing the result
+            sb+= ","
+        print(str(sb))
+        return sb
         
-    def dfs(self, root):
-        if not root:
-            self.sb += "#"
-            self.sb += ","
-            return
-        else:
-            self.sb += str(root.val)
-            self.sb += ","
-            self.dfs(root.left)
-            self.dfs(root.right)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -39,25 +50,29 @@ class Codec:
         :rtype: TreeNode
         """
         if not data: return None
+        # Splitting the data on commas
         strArr = data.split(",")
-        root = TreeNode(str(strArr[0]))
-        global i
-        i += 1
-        self.helper(root, strArr)
-        return root
+        # Creating Treenode out of string. First converting string node to int node
+        root = TreeNode(int(strArr[0]))
+        q = deque()
+        q.append(root) # root for parents
+        # Using i iterator for adding children to root
+        i = 1
+        while q and i < len(strArr):
+            curr = q.popleft()
+            # If we hit # just skip by incrementing i by 1
 
-    def helper(self, root, strArr):
-        global i
-        if i == len(strArr): return
-        if not root: return
-        if strArr[i] != "#":
-            root.left = TreeNode(int(strArr[i]))
-        i+=1
-        self.helper(root.left, strArr)
-        if strArr[i] != "#":
-            root.right = TreeNode(int(strArr[i]))
-        i+=1
-        self.helper(root.right, strArr)
+            # left
+            if strArr[i] != "#":
+                curr.left = TreeNode(int(strArr[i]))
+                q.append(curr.left)
+            i+=1
+            # right
+            if strArr[i] != "#":
+                curr.right = TreeNode(int(strArr[i]))
+                q.append(curr.right)
+            i+=1
+        return root
 
         
 
