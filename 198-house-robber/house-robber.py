a123dc1de3a1b3f2aa07da9997fb0c5fa293337f
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-10-16 09:35:38 -0700

    Time: 41 ms (18.65%) | Memory: 16.6 MB (36.91%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,4 +1,4 @@
-# SC - O(N)
+# SC - O(1)
 # TC - O(N)
 
 class Solution:
@@ -9,14 +9,18 @@ class Solution:
         if n == 1:  # If there is only one house, return its value
             return nums[0]
         
-        arr = [0] * n  # Create an array to store maximum amounts at each house
-        arr[0] = nums[0]  # The maximum amount from the first house is its own value
-        arr[1] = max(nums[0], nums[1])  # The maximum amount from the first two houses
+        
+        prev = nums[0]  # The maximum amount from the first house is its own value
+        curr = max(nums[0], nums[1])  # The maximum amount from the first two houses
 
         # Iterate through the remaining houses starting from the third one
         for i in range(2, n):
-            # Decide whether to rob the current house (nums[i]) + amount from two houses back (arr[i-2])
-            # or to skip the current house and take the amount from the previous house (arr[i-1])
-            arr[i] = max(arr[i-1], nums[i] + arr[i-2])
+            # our current changes and we want our prev to point at last curr
+            # hence we store curr in temp and prev goes to temp to keep track of past values
+            temp = curr
+            # We override curr with max value at that cell
+            curr = max(curr, nums[i] + prev)
+            # our prev moves to previous curr that is temp
+            prev = temp
         
-        return arr[-1]  # Return the maximum amount that can be robbed, which is stored in the last element of arr
+        return curr  # Return the maximum amount that can be robbed, which is stored in the last element of arr
