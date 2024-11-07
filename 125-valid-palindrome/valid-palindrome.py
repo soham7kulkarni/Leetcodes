02e38c6179991e01dda2423b52937dd1125b8f02
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-11-07 10:32:03 -0800

    Time: 23 ms (7.85%) | Memory: 16.8 MB (90.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,20 +4,16 @@ class Solution:
         p1 = 0
         p2 = len(s) - 1
 
-        while p1 < p2 :
+        while p1 < p2:
             while p1 < p2 and not self.isalnum(s[p1]):
                 p1 += 1
             while p2 > p1 and not self.isalnum(s[p2]):
-                p2 -= 1 
+                p2 -= 1
             if s[p1].lower() != s[p2].lower():
                 return False
-            else:
-                p1 += 1
-                p2 -= 1
-        return True 
-
-    def isalnum(self, c):
-        return(ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9')) 
-
-
+            p1 += 1
+            p2 -= 1
+        return True
+    def isalnum(self, i) -> bool:
+        return ord('a') <= ord(i) <= ord('z') or ord('A') <= ord(i) <= ord('Z') or ord('0') <= ord(i) <= ord('9')
         
