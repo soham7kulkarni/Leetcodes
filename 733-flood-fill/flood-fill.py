2d3c31994e03aab3383e0bebd6ef83b9e50a080e
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-11-08 11:23:23 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.16%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,24 +1,22 @@
-# TC - O(MN)
-# SC - O(MN)
-# Approach - DFS
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m = len(image)
         n = len(image[0])
-        original = image[sr][sc]
-        
-        if color == original: return image
         dirs = [[0,1], [0,-1], [-1, 0], [1, 0]]
-        self.dfs(image, dirs, original, color, m, n, sr, sc)
+        curr = image[sr][sc]
+        if curr == color:
+            return image
+        self.dfs(image, sr, sc, m, n, dirs, color, curr)
         return image
-
-    def dfs(self, image, dirs, original, color, m, n, sr, sc):
-
+    def dfs(self, image, sr, sc, m, n, dirs, color, curr):
         # base
-        if sr < 0 or sr == m or sc < 0 or sc == n or image[sr][sc] != original: return
+        if sr < 0 or sr >= m or sc < 0 or sc >= n or image[sr][sc] != curr: return  
         # logic
         image[sr][sc] = color
-        for direction in dirs:
-            nr = sr + direction[0]
-            nc = sc + direction[1]
-            self.dfs(image, dirs, original, color, m, n, nr, nc)
+        for dir in dirs:
+            nr = sr + dir[0]
+            nc = sc + dir[1]
+            self.dfs(image, nr, nc, m, n, dirs, color, curr)
+
+
+        
