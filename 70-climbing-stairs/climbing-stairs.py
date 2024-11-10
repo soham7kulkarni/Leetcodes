8e2c62fcfd56f995689865f482214347e51189df
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2024-11-10 15:37:29 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (18.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,29 +1,30 @@
-# Approach - For any step i, the number of distinct ways to reach that step 
-# can be derived from the sum of ways to reach the previous step 
-# and the step before that
-# you can either come from step i-1 
-# (taking a single step) or from step i-2 (taking two steps)
-
-# TC - O(N)
-# SC - O(1)
-
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # Check if n is zero. If there are no steps, there are zero ways to climb.
         if not n:
             return 0
+
+        # Base case: If there's only 1 step, there's only 1 way to reach it.
         if n == 1:
             return 1
-        
-        
-        # Base cases
-        first = 1  # 1 way to stay at the ground (do nothing)
-        second = 1  # 1 way to climb to the first step
-        
-        # Fill the dp array using the recurrence relation
+
+        # Initialize the first two values in the Fibonacci sequence for the staircase problem.
+        # 'first' represents the number of ways to reach the previous step.
+        # 'second' represents the number of ways to reach the current step.
+        first = 1
+        second = 1
+
+        # Iterate from step 2 up to n, calculating the number of ways to reach each step.
         for i in range(2, n + 1):
+            # Calculate the number of ways to reach the current step.
             current = first + second
+
+            # Move 'second' to 'first' to prepare for the next iteration,
+            # as 'first' should represent the previous step in the next round.
             second = first
+
+            # Update 'first' to 'current' as it will now represent the ways to reach the current step.
             first = current
-        
-        return current  # Return the number of ways
-        
+
+        # After the loop, 'current' holds the number of ways to reach the nth step.
+        return current
