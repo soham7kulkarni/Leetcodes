1c5fbe6673b539a4ff840a65e468510a2cb30f28
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2025-12-22 19:00:12 -0800

    Time: 7 ms (90.11%) | Memory: 17.7 MB (82.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,18 +1,17 @@
-# TC - O(N)
-# SC - O(1)
+# TC: O(n) -> Considering both are equal, O(n+m) -> whichever has max length
+# SC: O(1) -> 2 dicts with O(26), O(k) -> if k unicode chars are present
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t):
-            return False
+        if len(s) != len(t): return False
 
         s_dict = {}
         t_dict = {}
 
-        for i in range(len(s)):
-            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
-        for j in range(len(t)):
-            t_dict[t[j]] = 1 + t_dict.get(t[j], 0)
-        
+        for i in s:
+            s_dict[i] = 1 + s_dict.get(i, 0)
+        for i in t:
+            t_dict[i] = 1 + t_dict.get(i, 0)
+
         return s_dict == t_dict
         
