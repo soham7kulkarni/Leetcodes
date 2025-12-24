f3722494d7a770929920de365998e44275dc8158
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2025-12-23 19:58:58 -0800

    Time: 0 ms (100.00%) | Memory: 18.7 MB (63.57%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,13 @@
+# Approach 1 - iterating through all numbers for a specific number - O(n^2), O(1)
+# Approach 2 - to find pair, we have to find target - num1. Finding in hashmap means finding through keys. Store values in keys and their indexes as values of hashmap. Check if you have difference in hashmap. If yes, return pair or put the number value in key and index in value of hashmap. HASHMAP IS DICTIONARY IN PYTHON - O(N), O(N) 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for index, value in enumerate(nums):
-            difference = target - value
-            if difference in hashmap:
-                return [hashmap[difference], index]
-            hashmap[value] = index
+        prev = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prev:
+                return [prev[diff], i]
+            prev[n] = i
+        return
         
