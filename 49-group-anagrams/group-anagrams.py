8df18e3fb7ab422b708bdb0b810768c8bb5fd38d
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-03 20:11:28 -0800

    Time: 20 ms (25.90%) | Memory: 22.1 MB (28.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,12 @@
-# Approach - Instead of sorting each string (which takes O(Klog K)), this method uses a character count as a unique identifier for anagrams. We convert this list into a tuple (because lists are mutable and cannot be used as keys) and use it as a key in a defaultdict to group all strings that share that same frequency "signature".
-# TC - O(N.K)
-# SC - O(N.K)
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = defaultdict(list)
         for s in strs:
-            count = [0]*26
+            count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
             ans[tuple(count)].append(s)
         return list(ans.values())
+
         
