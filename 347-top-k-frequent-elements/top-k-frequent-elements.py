711dfa9c1735dbe739a47e404f523d10946b9193
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-03 19:56:34 -0800

    Time: 7 ms (53.25%) | Memory: 20.9 MB (85.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,17 @@
-# Approach - Calculate the frequency map of all num and then use min heap. Within min-heap, we will put only k elements. As we comapre frequency and push nums within heap, the lowest frequency nums will get replaced and at the end we have only k nums with highest frequency. We return only values within res
-# TC - O(nlogk)
-# SC - O(n + k)
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-
+        for i in nums:
+            count[i] = 1 + count.get(i, 0)
         heap = []
         for num in count.keys():
             heapq.heappush(heap, (count[num], num))
-            if len(heap) > k :
+            if len(heap) > k:
                 heapq.heappop(heap)
-        
         res = []
         for i in range(k):
             res.append(heapq.heappop(heap)[1])
-        
         return res
+        
 
         
