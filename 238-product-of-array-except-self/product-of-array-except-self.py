3d476e371abefa11dc749f1a52d27de5c58bb5af
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-03 19:43:37 -0800

    Time: 23 ms (63.50%) | Memory: 23.7 MB (40.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,14 @@
-# Approach 1 - two nested for loops and then when i == j, 
-# we will not do multiplication or else do multiplication. TC - O(N*N)
-# Approach 2 - multiplication of all elements to the left of that element 
-# in first for loop and 
-# all right in second pass. Multiply them with the resultant itself to save on space. 
-# Or else two result array and multiply them. 
-# O(N), O(1)
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        answer = [1] * n
-        running_product = 1
+        answer = [1]*n
+        rp = 1
         for i in range(1, n):
-            running_product = running_product*nums[i-1]
-            answer[i] = running_product
-        running_product = 1
-        for i in range(n-2, -1,-1):
-            running_product = running_product*nums[i+1]
-            answer[i] = answer[i]*running_product
-        return answer         
+            rp = rp * nums[i-1]
+            answer[i] = rp
+        rp = 1
+        for i in range(n-2, -1, -1):
+            rp = rp * nums[i+1]
+            answer[i] = answer[i] * rp
+        return answer
+        
