f7c53aa9f3db102d0f9dca26b4fb8161d77a22d4
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-04 17:23:32 -0800

    Time: 68 ms (24.03%) | Memory: 34.7 MB (14.60%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,20 +1,3 @@
-# Brute Force - sort the numbers and traverse to check longest sequence
-# TC - O(nlogn)
-
-# Optimal - Put elements in the set
-# for each number, check if i-1 exists
-# if yes, already part of sequence, skip (we skip 4 when 1,2,3 are present)
-# if no, reset count and longest to 1(we start processing 1 and increment)
-# start finding next elements
-# updating longest using count variable
-
-# TC - O(N), exact 2N, 
-# suppose we find 1 at last
-# then we have already traversed array omce and we will go theough array
-# once again to find sequence for last element(1)
-# SC - O(N)
-
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if not nums:
@@ -26,9 +9,9 @@ class Solution:
             if i-1 not in arr:
                 count = 1
                 x = i
-                while x + 1 in arr:
+                while x+1 in arr:
                     count += 1
                     x += 1
-                    longest = max(count, longest)
+                    longest = max(longest, count)
         return longest
         
