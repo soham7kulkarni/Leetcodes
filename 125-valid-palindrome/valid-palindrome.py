65ca6486806a691dbf9b1612d5b40a89ea3bfce7
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-05 19:09:31 -0800

    Time: 19 ms (8.61%) | Memory: 17.8 MB (85.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,3 +1,5 @@
+# Defining isalnum function on my own. Checking if ASCII value of character belongs to specific range in ASCII spectrum. If not increment and decrement left and right pointers to skip such characters. Or else convert them to lowercase and compare two charcters till two pointers cross each other.
+# TC - O(N/2) i.e. O(N), SC - O(1). Not using any extra memory
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         p1 = 0
@@ -15,5 +17,5 @@ class Solution:
         return True
 
     def isalnum(self, i) -> bool:
-        return ord('a') <= ord(i) <= ord('z') or ord('A') <= ord(i) <= ord('Z') or ord('0') <= ord(i) <= ord('9') 
+        return ord('a') <= ord(i) <= ord('z') or ord('A') <= ord(i) <= ord('Z') or ord('0') <= ord(i) <= ord('9')
         
