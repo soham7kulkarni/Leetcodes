596b76b10fea9bd49772aa546900ef236cc49a6d
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-06 19:47:17 -0800

    Time: 3 ms (80.91%) | Memory: 20.6 MB (7.94%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,20 +1,15 @@
-# Approach 1 - checking combination of each element with every other element in array. O(N*N)
-# Approach 2 - Two Pointers. take sum. increase or decrease left or right pointers aacording to sum.
-# TC - O(N), SC - O(1)
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        p1 = 0
-        p2 = len(numbers) - 1
-        ans = []
-        while p1 < p2:
-            if numbers[p1] + numbers[p2] == target:
-                ans.append(p1+1)
-                ans.append(p2+1)
-                break 
-            while numbers[p1] + numbers[p2] < target:
-                p1 += 1
-            while numbers[p1] + numbers[p2] > target:
-                p2 -= 1
-        return ans
-            
+        n = len(numbers)
+        low = 0
+        high = n-1
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low+1, high+1]
+            elif total < target:
+                low += 1
+            else:
+                high -= 1
+
         
