23edb6665fdc047d1909006c60c960ff3d90670d
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-06 19:58:16 -0800

    Time: 19 ms (8.55%) | Memory: 19.6 MB (18.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,17 @@
-# Defining isalnum function on my own. Checking if ASCII value of character belongs to specific range in ASCII spectrum. If not increment and decrement left and right pointers to skip such characters. Or else convert them to lowercase and compare two charcters till two pointers cross each other.
-# TC - O(N/2) i.e. O(N), SC - O(1). Not using any extra memory
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        p1 = 0
-        p2 = len(s) - 1
-
-        while p1 < p2:
-            while p1 < p2 and not self.isalnum(s[p1]):
-                p1 += 1
-            while p2 > p1 and not self.isalnum(s[p2]):
-                p2 -= 1
-            if s[p1].lower() != s[p2].lower():
+        n = len(s)
+        low = 0
+        high = n - 1
+        while low < high:
+            while low < high and not self.isalnum(s[low]):
+                low += 1
+            while high > low and not self.isalnum(s[high]):
+                high -= 1
+            if s[low].lower() != s[high].lower():
                 return False
-            p1 += 1
-            p2 -= 1
+            low += 1
+            high -= 1
         return True
-
     def isalnum(self, i) -> bool:
         return ord('a') <= ord(i) <= ord('z') or ord('A') <= ord(i) <= ord('Z') or ord('0') <= ord(i) <= ord('9')
-        
