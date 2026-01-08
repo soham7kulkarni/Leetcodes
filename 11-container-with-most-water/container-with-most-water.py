3111f26f60e33c16497baffbbb39696da082d016
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-07 19:46:09 -0800

    Time: 62 ms (93.56%) | Memory: 29.9 MB (16.39%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,25 +1,17 @@
-# TC - O(N)
-# SC - O(1)
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        i = 0  # Initialize the left pointer
-        j = len(height) - 1  # Initialize the right pointer
-        maxArea = 0  # Variable to keep track of the maximum area
-
-        # Continue until the two pointers meet
-        while i < j:
-            length = j - i  # Calculate the width of the container
-            currentHeight = min(height[i], height[j])  # Find the height of the container
-            area = length * currentHeight  # Calculate the area of the container
-            
-            # Update the maximum area found so far
-            maxArea = max(maxArea, area)
-
-            # Move the pointer pointing to the shorter line
-            if height[i] < height[j]:
-                i += 1  # Move the left pointer to the right
+        if not height:
+            return 0
+        left = 0
+        right = len(height)-1
+        area = 0
+        while left < right:
+            l = right - left
+            h = min(height[left], height[right])
+            water = l*h
+            area = max(area, water)
+            if height[left] < height[right]:
+                left += 1
             else:
-                j -= 1  # Move the right pointer to the left
-
-        return maxArea  # Return the maximum area found
+                right -= 1
+        return area
