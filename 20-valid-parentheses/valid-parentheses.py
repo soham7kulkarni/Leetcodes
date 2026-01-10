f338299f4338e486c674f8ecf47fcf3787509b9b
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-09 19:05:16 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,10 @@
-# TC - O(N), SC - O(1)
-# Creating dictionary and checking the match of closing characters 
-# with their opening character
 class Solution:
     def isValid(self, s: str) -> bool:
         st = []
-        closeToOpen = {")":"(", "}":"{", "]":"["}
+        toClose = {')':'(','}':'{',']':'['}
         for i in s:
-            if i in closeToOpen:
-                if st and st[-1] == closeToOpen[i]:
+            if i in toClose:
+                if st and st[-1] == toClose[i]:
                     st.pop()
                 else:
                     return False
