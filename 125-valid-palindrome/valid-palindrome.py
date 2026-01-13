60e69eb759383ac19a63392ecbd8278d9c9476fb
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-01-12 19:29:38 -0800

    Time: 19 ms (8.43%) | Memory: 19.7 MB (24.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         n = len(s)
-        low = 0
-        high = n - 1
-        while low < high:
-            while low < high and not self.isalnum(s[low]):
-                low += 1
-            while high > low and not self.isalnum(s[high]):
-                high -= 1
-            if s[low].lower() != s[high].lower():
+        i = 0
+        j = n-1
+        while i < j:
+            while i < j and not self.isalnum(s[i]):
+                i+=1
+            while j > i and not self.isalnum(s[j]):
+                j-=1
+            if s[i].lower() != s[j].lower():
                 return False
-            low += 1
-            high -= 1
+            i+=1
+            j-=1
         return True
-    def isalnum(self, i) -> bool:
+    def isalnum(self,i) -> bool:
         return ord('a') <= ord(i) <= ord('z') or ord('A') <= ord(i) <= ord('Z') or ord('0') <= ord(i) <= ord('9')
+
+        
