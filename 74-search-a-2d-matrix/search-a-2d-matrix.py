d565dc0ba2921fa73ec301d5131730e1cf3566b6
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-07-30 17:47:17 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (38.07%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -3,16 +3,15 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         low = 0
-        high = (m*n)-1
+        high = m*n - 1
         while low <= high:
             mid = low + (high-low)//2
             r = mid//n
             c = mid%n
-            if matrix[r][c] == target:
-                return True
-            elif matrix[r][c] > target:
-                high = mid - 1
-            else :
-                low = mid + 1
+            if matrix[r][c] == target: return True
+            elif matrix[r][c] < target :
+                low = mid+1
+            else:
+                high=mid-1
         return False
         
