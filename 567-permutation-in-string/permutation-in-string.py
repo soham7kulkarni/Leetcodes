ee8b4648b64b910ce95704a8cde2f88c39e45466
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-09-10 18:31:36 -0700

    Time: 11 ms (96.36%) | Memory: 19.3 MB (54.39%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,27 +3,23 @@ class Solution:
         if len(s1) > len(s2):
             return False
         
-        s1Count = [0]*26
-        s2Count = [0]*26
+        s1count = [0]*26
+        s2count = [0]*26
 
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
-           
+            s1count[ord(s1[i]) - ord('a')] += 1
+            s2count[ord(s2[i]) - ord('a')] += 1
+
         for i in range(len(s1), len(s2)):
-            if s1Count == s2Count:
+            if s1count == s2count:
                 return True
 
-            s2Count[ord(s2[i]) - ord('a')] += 1
-            s2Count[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            s2count[ord(s2[i]) - ord('a')] += 1
+            s2count[ord(s2[i - len(s1)]) - ord('a')] -= 1
 
-        if s1Count == s2Count:
+        if s1count == s2count :
             return True
-        else:
-            return False
-
-            
-
-                
+        
+        return False
 
         
