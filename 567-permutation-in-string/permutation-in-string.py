595942edcd145a34ebd1317c36786aed99062174
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-09-11 18:48:46 -0700

    Time: 11 ms (96.34%) | Memory: 19.3 MB (54.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,25 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+        if len(s1) > len(s2) :
+             return False
         
-        s1count = [0]*26
-        s2count = [0]*26
+        counts1 = [0]*26
+        counts2 = [0]*26
 
         for i in range(len(s1)):
-            s1count[ord(s1[i]) - ord('a')] += 1
-            s2count[ord(s2[i]) - ord('a')] += 1
-
-        for i in range(len(s1), len(s2)):
-            if s1count == s2count:
-                return True
-
-            s2count[ord(s2[i]) - ord('a')] += 1
-            s2count[ord(s2[i - len(s1)]) - ord('a')] -= 1
-
-        if s1count == s2count :
-            return True
+            counts1[ord(s1[i]) - ord('a')] += 1
+            counts2[ord(s2[i]) - ord('a')] += 1
         
+        for i in range(len(s1), len(s2)):
+            if counts1 == counts2:
+                return True
+            counts2[ord(s2[i]) - ord('a')] += 1
+            counts2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        if counts1 == counts2:
+            return True
         return False
-
         
