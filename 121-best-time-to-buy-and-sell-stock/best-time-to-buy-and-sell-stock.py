3de48358e19c192e32fd8929bcf445811a306173
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-09-11 17:57:47 -0700

    Time: 61 ms (28.51%) | Memory: 28.6 MB (42.20%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         smallest = prices[0]
-        maximum = 0
+        profit = 0
         for i in range(1, len(prices)):
             smallest = min(smallest, prices[i])
-            maximum = max(maximum, prices[i]-smallest)
-        return maximum
+            profit = max(profit, prices[i]-smallest)
+        return profit
         
