07c96ad8b4c4a18d3951934caf013ddc05b3bda0
Soham Kulkarni <63456618+soham7kulkarni@users.noreply.github.com>
2026-09-11 18:26:34 -0700

    Time: 63 ms (96.62%) | Memory: 19.8 MB (16.08%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,16 +3,17 @@ class Solution:
         left = 0
         maxCount = 0
         maxLength = 0
-        count = defaultdict(int)
+        countDict = defaultdict(int)
 
         for right in range(len(s)):
-            count[s[right]] += 1
-            maxCount = max(maxCount, count[s[right]])
+            countDict[s[right]] += 1
+            maxCount = max(maxCount, countDict[s[right]])
 
             if (right - left + 1) - maxCount > k:
-                count[s[left]] -= 1
+                countDict[s[left]] -= 1
                 left += 1
+            maxLength = max(maxLength, right - left + 1)
 
-            maxLength = max(maxLength, right-left+1)
-        return maxLength 
-        
+        return maxLength
+
+
